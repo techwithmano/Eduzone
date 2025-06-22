e1c45000c49317db73a18c2618f25d45d34dc991
3bodyyy <abdukhogali1@gmail.com>
2025-06-22 22:02:00 +0000

    2025-06-22T19:26:48Z [web] <IDX> Starting preview... 2025-06-22T19:26:48

---
+++ next.config.js
@@ -0,0 +1,21 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
